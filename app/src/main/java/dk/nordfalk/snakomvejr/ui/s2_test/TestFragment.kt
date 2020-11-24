d8c47e6cc0ac37198cbe402b05917ed0a4bf7f69
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2020-11-24 14:11:52 +0100

    TestFragment benytter nu talesyntese

--- app/src/main/java/dk/nordfalk/snakomvejr/ui/s2_test/TestFragment.kt
+++ app/src/main/java/dk/nordfalk/snakomvejr/ui/s2_test/TestFragment.kt
@@ -6,7 +6,6 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.OvershootInterpolator
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -17,6 +16,9 @@ import kotlinx.android.synthetic.main.s2_test_fragment.*
 
 class TestFragment : Fragment() {
 
+
+    private var latest: Long = 0
+    private lateinit var tts: VejrTekstTilTale
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -36,6 +38,7 @@ class TestFragment : Fragment() {
             textView2.setHint(if (it==null || it.length==0) "Listening..." else "")
             println("speechDataCallback $it")
             if (it?.toLowerCase()!!.contains("vejr")) {
+                vejrSpeechListener.stop()
                 startVejr()
             }
         }
@@ -45,10 +48,6 @@ class TestFragment : Fragment() {
 
         micButton.setOnTouchListener(object : View.OnTouchListener {
             override fun onTouch(view: View?, motionEvent: MotionEvent): Boolean {
-                if (motionEvent.action == MotionEvent.ACTION_UP) {
-                    vejrSpeechListener.stop();
-                    micButton.animate().scaleX(1f).scaleY(1f).setDuration(100)
-                }
                 if (motionEvent.action == MotionEvent.ACTION_DOWN) {
                     micButton.setImageResource(R.drawable.ic_baseline_hearing_48)
                     micButton.animate().scaleX(1.5f).scaleY(1.5f).setInterpolator(OvershootInterpolator())
@@ -57,9 +56,16 @@ class TestFragment : Fragment() {
                         }
                     vejrSpeechListener.start();
                 }
+                if (motionEvent.action == MotionEvent.ACTION_UP) {
+                    vejrSpeechListener.stop();
+                    micButton.animate().scaleX(1f).scaleY(1f).setDuration(100)
+                }
                 return false
             }
         })
+
+        tts =
+            VejrTekstTilTale(requireContext());
 
 
         return root
@@ -73,6 +79,10 @@ class TestFragment : Fragment() {
     }
 
     private fun startVejr() {
+        val now = System.currentTimeMillis();
+        if (now - latest<2000) return  //undgå flere kald kort tid efter hinanden
+        latest = now
+
         val w = WeatherData()
         weatherAskedImageView.visibility = View.VISIBLE
         weatherAskedTextView.visibility = View.VISIBLE
@@ -81,7 +91,10 @@ class TestFragment : Fragment() {
 
         w.getDescription({
             println(it)
-            weatherResultTextView.post { weatherResultTextView.text = it }
+            weatherResultTextView.post {
+                weatherResultTextView.text = it
+                tts.speak(it)
+            }
         })
 
     }
---
+++ app/src/main/java/dk/nordfalk/snakomvejr/ui/s2_test/VejrTekstTilTale.java
@@ -0,0 +1,49 @@
+package dk.nordfalk.snakomvejr.ui.s2_test;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+import android.view.inputmethod.InputMethodManager;
+
+import java.util.Locale;
+
+public class VejrTekstTilTale implements TextToSpeech.OnInitListener {
+    private boolean initialiseret;
+    private TextToSpeech tts;
+    private String pendingText;
+
+    public VejrTekstTilTale(Context ctx) {
+        tts = new TextToSpeech(ctx, this);
+        initialiseret = false;
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            initialiseret = true;
+            int res = tts.setLanguage(new Locale("da", ""));
+            if (res == TextToSpeech.LANG_MISSING_DATA || res == TextToSpeech.LANG_NOT_SUPPORTED) {
+                res = tts.setLanguage(Locale.getDefault());
+                if (res == TextToSpeech.LANG_MISSING_DATA || res == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    res = tts.setLanguage(Locale.US);
+                    if (res == TextToSpeech.LANG_MISSING_DATA || res == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        initialiseret = false;
+                        System.out.println("Kunne ikke initialisere TTS");
+                    }
+                }
+            }
+
+
+            if (initialiseret) {
+                Locale sprog = tts.getVoice().getLocale();
+                if (pendingText!=null) speak(pendingText);
+                else speak("Tekst til tale klar for sproget " + sprog.getDisplayLanguage(sprog));
+            }
+        }
+    }
+
+    public void speak(String text) {
+        System.out.println("speak text = " + text);
+        if (initialiseret) tts.speak(text, TextToSpeech.QUEUE_ADD, null, null);
+        else pendingText = text;
+    }
+}
