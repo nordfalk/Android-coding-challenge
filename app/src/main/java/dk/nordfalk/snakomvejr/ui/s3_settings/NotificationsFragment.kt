c0faab040b5f0663933b63b2a0a1ae571589ad4d
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2020-11-24 12:42:57 +0100

    navngivning

--- app/src/main/java/dk/nordfalk/snakomvejr/ui/s3_settings/NotificationsFragment.kt
+++ app/src/main/java/dk/nordfalk/snakomvejr/ui/s3_settings/NotificationsFragment.kt
@@ -21,7 +21,7 @@ class NotificationsFragment : Fragment() {
     ): View? {
         notificationsViewModel =
                 ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.s3_notifications_fragment, container, false)
+        val root = inflater.inflate(R.layout.s3_settings_fragment, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
